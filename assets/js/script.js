71cb2c11125301a0d6a6d8cdbd358e4e12e2c860
Omar <omarta@code.edu.az>
2024-02-09 22:10:53 +0400

    ...

---
+++ assets/js/script.js
@@ -0,0 +1,101 @@
+"use strict";
+
+
+// let word = "abc";
+
+// function reverse(word) {
+//     let reversed = "";
+//     for (let i = word.length - 1; i >= 0; i--) {
+//     reversed += word[i];
+//     }
+//     console.log(reversed);
+// }
+
+
+// reverse(word);
+
+
+
+
+
+// let age = 16;
+
+// function yearOfBirth(age) {
+//     let year = 2024 - age;
+//     console.log(year);
+// }
+
+// yearOfBirth(age);
+
+
+
+
+
+// const person1 = {
+//     id: 1,
+//     name: 'person1',
+//     fullName: 'person1 surname1',
+//     age: 18,
+//     salary: 400
+// };
+
+// const person2 = {
+//     id: 2,
+//     name: 'person2',
+//     fullName: 'person2 surname2',
+//     age: 26,
+//     salary: 203
+// };
+// const person3 = {
+//     id: 3,
+//     name: 'person3',
+//     fullName: 'person3 surname3',
+//     age: 16,
+//     salary: 200
+// };
+
+
+// const people = [person1, person2, person3];
+
+
+
+// function count(people) {
+//     let count = 0
+//     people.forEach(person => {
+//         count ++;
+//     });
+
+//     console.log(count);
+// }
+
+
+// count(people);
+
+
+// function averageSalary(people) {
+//     let count = 0
+//     let allSalary = 0
+//     people.forEach(person => {
+//         count ++;
+//         allSalary += person.salary
+//     });
+
+//     let average = allSalary / count
+//     console.log(average);
+// }
+
+
+// averageSalary(people);
+
+
+// function searchPerson(people) {
+//     people.forEach(person => {
+//         if(person.fullName === 'person1 surname1'){
+//             console.log(person.fullName);
+//         }
+       
+//     });
+// }
+
+
+// searchPerson(people);
